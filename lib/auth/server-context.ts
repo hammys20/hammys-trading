6f2b2fc874cbe3befc9f9e9393f4391ed9506eaf
hammys20 <hammys.trading@gmail.com>
2026-01-29 15:04:05 -0500

    Updated server contxt

--- lib/auth/server-context.ts
+++ lib/auth/server-context.ts
@@ -1,20 +1,27 @@
 import { cookies } from "next/headers";
 import { fetchAuthSession } from "aws-amplify/auth/server";
+import { runWithAmplifyServerContext } from "aws-amplify/adapter-nextjs";
 
 export async function isAdminServer(): Promise<boolean> {
   try {
-    const session = await fetchAuthSession({
-      cookies,
-    });
+    const session = await runWithAmplifyServerContext(
+      {
+        nextServerContext: { cookies },
+      },
+      async () => {
+        return await fetchAuthSession();
+      }
+    );
 
     const groups =
-      session.tokens?.accessToken?.payload["cognito:groups"];
+      session.tokens?.accessToken?.payload?.["cognito:groups"];
 
     return Array.isArray(groups) && groups.includes("Admin");
   } catch {
     return false;
   }
 }
+
 
 
 

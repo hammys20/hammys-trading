83fe9c826dfdfcea0b1ce9e9d88b14ad3adc7407
hammys20 <hammys.trading@gmail.com>
2026-01-29 21:42:34 -0500

    Use explicit .ts imports for Amplify Gen2 backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,9 @@
 // amplify/backend.ts
 import { defineBackend } from "@aws-amplify/backend";
 
-import { auth } from "./auth/resource";
-import { data } from "./data/resource";
-import { storage } from "./storage/resource";
+import { auth } from "./auth/resource.ts";
+import { data } from "./data/resource.ts";
+import { storage } from "./storage/resource.ts";
 
 defineBackend({
   auth,
